b828d2d61caef507da746e7f264c738d1e09119a
ifetayo <ifetayo.agunbiade@gmail.com>
2016-05-24 05:25:48 +0100

    fixed performance issue with binding foreground with text box and also changed the path to pathicon

--- WinPhone8.1TwitterCustomPanel/ControlPanel/SplitPanel.cs
+++ WinPhone8.1TwitterCustomPanel/ControlPanel/SplitPanel.cs
@@ -9,26 +9,51 @@ using Windows.UI.Xaml.Controls;
 
 namespace WinPhone8._1TwitterCustomPanel.ControlPanel
 {
+    /// <summary>
+    /// This class is responsible for providing a custom control layout 
+    /// for the items that act as navigation headers for the listbox.
+    /// </summary>
+    /// <remarks>
+    /// The name of this custom control is SplitPanel (class name)
+    /// This class specifies the layout template for the control it is attached
+    /// to. Basically gives us full control as to the size interms of height and
+    /// width of each element, in our case the elements are the items in a 
+    /// listbox
+    /// What this class simply does is compute and assign a size to each item
+    /// element in the list box.
+    /// In order to have full control of the layout we 
+    /// override the MeasureOverride and ArrangeOverride, each called one 
+    /// after the order in the given order.
+    /// </remarks>
     public class SplitPanel : Panel
     {
-        private List<Size> computedSizes;
-        double childSize = 0;
-
+        /// <summary>
+        /// This is the first of the two overriden methods to be called
+        /// In this method we specify the size for each item in the listbox
+        /// </summary>
+        /// <param name="availableSize"> The available size for the listbox
+        /// That is the parent container for the items, this is the space each
+        /// child of the listbox has to share.</param>
+        /// <returns>
+        /// In our case it returns the size (height and width) </returns>
         protected override Size MeasureOverride(Size availableSize)
         {
-            // the final measure size is the available size for the width, and the maximum
-            // desired size of our children for the height
+            // the final measure size is the available size for the width,
+            // and height for the container children
             Size finalSize = new Size { Width = availableSize.Width };
-            double kjk = this.ActualHeight;
-
-            computedSizes = new List<Size>();
 
             if (this.Children.Count != 0)
             {
+                //here we divide the available width by the number of child items
+                //in our case each child get an equal amount of space
+                //example lets say the available width is 4, and we have 4 children, 
+                //after this computation each child will have a with of 1 (4/4 = 1).
                 availableSize.Width /= ((double)this.Children.Count);
             }
+            //iterate through the elements in the listbox
             foreach (var current in this.Children)
             {
+                //give the child the available size as computed above
                 current.Measure(availableSize);
                 Size desiredSize = current.DesiredSize;
                 finalSize.Height = Math.Max(finalSize.Height, desiredSize.Height);
@@ -43,11 +68,23 @@ namespace WinPhone8._1TwitterCustomPanel.ControlPanel
             return finalSize;
         }
 
+        /// <summary>
+        /// This is the second (last) of the two overriden methods to be called
+        /// In this method we specify how each item is to be positioned in the listbox
+        /// </summary>
+        /// <param name="arrangeSize"> The size for the listbox
+        /// That is the parent container for the items, this is the space each child of the listbox has to share.</param>
+        /// <returns>
+        /// In our case it return the size </returns>
         protected override Size ArrangeOverride(Size arrangeSize)
         {
+            //create a rectangle the size of the listbox container
             Rect finalRect = new Rect(new Point(), arrangeSize);
+            
             double width = arrangeSize.Width / (this.Children.Count);
-
+            
+            //divide the rectangle into the number of children
+            //each having equal width as computed in the code statment above
             foreach (var child in this.Children)
             {
                 finalRect.Height = Math.Max(arrangeSize.Height, child.DesiredSize.Height);
@@ -58,7 +95,6 @@ namespace WinPhone8._1TwitterCustomPanel.ControlPanel
                 // move each child by the width increment 
                 finalRect.X += width;
             }
-
             return arrangeSize;
         }
     }
